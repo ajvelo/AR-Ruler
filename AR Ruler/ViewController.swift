ae4d551def351124523cdbd8ef8d9f131fbed287
Andreas Velounias <andreasvelounias@andreass-mbp-2.lan>
2017-12-30 13:14:22 +0000

    Formatted Distance.

--- AR Ruler/ViewController.swift
+++ AR Ruler/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     @IBOutlet var sceneView: ARSCNView!
     
     var dotNodes = [SCNNode]()
+    var textNode = SCNNode()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +23,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
-        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        //sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -78,16 +79,20 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         let distance = sqrt(pow(a, 2) + pow(b, 2) + pow(c, 2))
         
-        updateText(text: "\(distance)", atPosition: end.position)
+        let formattedDistance = String(format: "%.3f", distance) + "m"
+        
+        updateText(text: formattedDistance, atPosition: end.position)
     }
     
     func updateText(text: String, atPosition position: SCNVector3) {
+        
+        textNode.removeFromParentNode()
         
         let textGeometry = SCNText(string: text, extrusionDepth: 1.0)
         
         textGeometry.firstMaterial?.diffuse.contents = #colorLiteral(red: 0.5807225108, green: 0.066734083, blue: 0, alpha: 1)
         
-        let textNode = SCNNode(geometry: textGeometry)
+        textNode = SCNNode(geometry: textGeometry)
         
         textNode.position = SCNVector3(position.x, position.y + 0.01, position.z)
         
@@ -99,6 +104,15 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if dotNodes.count >= 2 {
+            
+            for dot in dotNodes {
+                dot.removeFromParentNode()
+            }
+            
+            dotNodes = [SCNNode]()
+        }
         
         if let touchLocation = touches.first?.location(in: sceneView) {
             
